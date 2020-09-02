ba20cc2edc4bd4277a39c8d1f439f0208bf5ec3d
Antonio Barcelos <antonio.barcelos@neotechnology.com>
2020-09-02 14:32:06 +0200

    Add spring web application with build script

---
+++ src/main/kotlin/com/neo4j/movies/MoviesApplication.kt
@@ -0,0 +1,11 @@
+package com.neo4j.movies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoviesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoviesApplication>(*args)
+}
---
+++ src/test/kotlin/com/neo4j/movies/MoviesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.neo4j.movies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
