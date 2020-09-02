3aa87d846b59f030bc2e74acd419c759103f6841
Antonio Barcelos <antonio.barcelos@neotechnology.com>
2020-09-02 21:05:06 +0200

    Getting actor from cast

--- src/main/kotlin/com/neo4j/movies/service/GetPeopleRelatedToMovieService.kt
+++ src/main/kotlin/com/neo4j/movies/service/GetPeopleRelatedToMovieService.kt
@@ -6,11 +6,20 @@ import com.neo4j.movies.businessrule.provider.GetPeopleRelatedToMovie
 import org.neo4j.driver.Driver
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.stream.Collectors
 
 @Service
 class GetPeopleRelatedToMovieService(@Autowired private val driver: Driver) : GetPeopleRelatedToMovie {
     override fun execute(input: GetPeopleRelatedToMovie.Input): GetPeopleRelatedToMovie.Output {
-        return GetPeopleRelatedToMovie.Output(
-                listOf(Pair(Relationship.ACTED_IN, Person(name="Antonio", born = 1986))))
+        return driver.session().readTransaction { it ->
+            val actors = it.run("MATCH (p:Person)-[r:ACTED_IN]->(m:Movie {title: \$movieName}) return p",
+                    mapOf(Pair("movieName", input.movieName)))
+                    .list()
+                    .stream()
+                    .map { record -> record.get("p").asNode() }
+                    .map { node -> Pair(Relationship.ACTED_IN, Person(node.get("name").asString(), node.get("born").asInt())) }
+                    .collect(Collectors.toList())
+            return@readTransaction GetPeopleRelatedToMovie.Output(actors)
+        }
     }
 }
